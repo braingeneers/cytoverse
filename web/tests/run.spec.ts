6fff8cce7f26f54a3b445a7d2576e8ca968bd60c
Rob Currie <rcurrie@ucsc.edu>
2025-07-23 18:19:34 -0500

    Add run test that doesn't wait for development workflows

--- web/tests/run.spec.ts
+++ web/tests/run.spec.ts
@@ -1,5 +1,15 @@
 import { test, expect } from '@playwright/test'
 
+test('Navigate to root, do NOT wait for results', async ({ page }) => {
+  await page.goto('/')
+  await page.getByTestId('radio-cpu-option').check()
+
+  // NOTE: Playwright reads the entire file so in practice only use MB size files...
+  const fileInputElement = page.locator('[type="file"]')
+  await fileInputElement.setInputFiles('tests/GSE136831_subsample_10.h5ad')
+  await page.getByTestId('run-stop-button').click()
+})
+
 test('Navigate to root, click run, wait for results', async ({ page }) => {
   await page.goto('/')
   await page.getByTestId('radio-cpu-option').check()
