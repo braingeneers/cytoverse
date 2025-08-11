1ccf962b712538b5b41a91fff3c2df1c65cf2dea
Rob Currie <rcurrie@ucsc.edu>
2025-08-11 11:28:41 -0600

    Update playwright tests

--- tests/e2e/run.spec.ts
+++ tests/e2e/run.spec.ts
@@ -25,21 +25,21 @@ test('Navigate to root, do NOT wait for results', async ({ page }) => {
 
 test('Navigate to root, click run, wait for results', async ({ page }) => {
   await page.goto('/')
-  await page.getByTestId('radio-cpu-option').check()
+  await page.getByTestId('radio-cpu-option').click()
 
   // NOTE: Playwright reads the entire file so in practice only use MB size files...
   const fileInputElement = page.locator('[type="file"]')
   await fileInputElement.setInputFiles('tests/fixtures/GSE136831_subsample_100.h5ad')
 
   await page.getByTestId('run-stop-button').click()
-  await expect(page.getByTestId('status')).toHaveText(/Complete/, {
+  await expect(page.getByTestId('status')).toHaveText(/Finished/, {
     timeout: 60000, // Wait up to 60 seconds for the status to change
   })
 })
 
 test('Open non-h5ad file and show error', async ({ page }) => {
   await page.goto('/')
-  await page.getByTestId('radio-cpu-option').check()
+  await page.getByTestId('radio-cpu-option').click()
 
   // NOTE: Playwright reads the entire file so in practice only use MB size files...
   const fileInputElement = page.locator('[type="file"]')
